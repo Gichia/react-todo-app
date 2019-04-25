f8ef31f7a0f13e7428537090f32bd31d7b2141a6
Gichia <petergichia35@gmail.com>
2019-04-25 12:27:28 +0300

    Updates

---
+++ precache-manifest.fa3103cc1dacc137c230bc25a288b24a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba7f545f0afaa4e54aca",
+    "url": "/react-todo-app/static/css/main.f6998f4c.chunk.css"
+  },
+  {
+    "revision": "ba7f545f0afaa4e54aca",
+    "url": "/react-todo-app/static/js/main.1645181a.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  },
+  {
+    "revision": "01a9540bb6feb2c963ec",
+    "url": "/react-todo-app/static/js/2.96f8e8de.chunk.js"
+  },
+  {
+    "revision": "ec9c8c395afcab91836378dd10fde6a0",
+    "url": "/react-todo-app/index.html"
+  }
+];
